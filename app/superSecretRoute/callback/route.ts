bca75782fb92dc5cb176ff29e4aeae2dd530ec5d
RazzorPets <138141472+Razzor-Pets@users.noreply.github.com>
2025-11-27 06:31:55 +0530

    fixed the thang again

--- app/superSecretRoute/callback/route.ts
+++ app/superSecretRoute/callback/route.ts
@@ -1,6 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
+'use server'
 
-export default function GET(request: NextRequest) {
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+import querystring from "querystring";
+
+export async function GET(request: NextRequest) {
     const code = request.nextUrl.searchParams.get("code");
     const state = request.nextUrl.searchParams.get("state");
     const stateCookie = request.cookies.get("spotify_auth_state");
@@ -9,5 +13,27 @@ export default function GET(request: NextRequest) {
         return NextResponse.json({ message: "Invalid request" }, { status: 400 });
     }
 
-    return NextResponse.json({ message: "Callback", code, state, stateCookie } , { status: 200 });
+    try{
+        
+        const response = await axios.post("https://accounts.spotify.com/api/token", querystring.stringify({
+            grant_type: "authorization_code",
+            code: code,
+            redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+        }), {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": `Basic ${Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString("base64")}`
+            }
+        });
+
+        const { access_token, refresh_token, expires_in } = response.data;
+        
+        return NextResponse.json({ message: "Callback", code, state, stateCookie, access_token, refresh_token, expires_in } , { status: 200 });
+
+    }catch(error){
+        console.log(error);
+        return NextResponse.json({ message: "Error", error } , { status: 500 });
+    }
+
+    
 }
